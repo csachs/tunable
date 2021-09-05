d418d3df2ff5cfe42e52ffb3c7812657f1b8e097
Christian Sachs <sachs.christian@gmail.com>
2021-09-05 16:57:58 +0200

    Version bump, changed to flit

--- tunable/__init__.py
+++ tunable/__init__.py
@@ -3,4 +3,22 @@
 documentation
 """
 
-from .tunableselectable import *
+from .tunableselectable import (
+    ModuleHelper,
+    Selectable,
+    SelectableManager,
+    Tunable,
+    TunableError,
+    TunableManager,
+)
+
+__version__ = '0.0.1.dev7'
+
+__all__ = [
+    "ModuleHelper",
+    "Selectable",
+    "SelectableManager",
+    "Tunable",
+    "TunableError",
+    "TunableManager",
+]
